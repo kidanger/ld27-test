11b8411cc96c6a2d2dead581f6dd88128c021a66
kidanger <angerj.dev@gmail.com>
2013-08-20 12:47:05 +0200

    update script

--- update_page.sh
+++ update_page.sh
@@ -8,6 +8,7 @@ cd ld27-test
 git checkout gh-pages
 cp ../drystal/build-web/*compress* .
 cp ../drystal/build-web/index.html .
+du -sh *
 git commit index.html *compress* -m "update pages"
 git push origin gh-pages
 git checkout master
