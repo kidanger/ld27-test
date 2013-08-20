536b46e6053c679a46f8cda2a0717cd1a7cef7d2
kidanger <angerj.dev@gmail.com>
2013-08-20 12:44:29 +0200

    fix upload

--- update_page.sh
+++ update_page.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 cd ../drystal
+./runner.py clean
 ./runner.py ld27-test repack
 cd ld27-test
 
 git checkout gh-pages
 cp ../drystal/build-web/*compress* .
+cp ../drystal/build-web/index.html .
 git commit index.html *compress* -m "update pages"
 git push origin gh-pages
 git checkout master
